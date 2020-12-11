d7c9b40c5b142dda58a6b1d38a06c4ecbf1e834a
Jack Hayford <jdhayford@gmail.com>
2020-12-10 20:39:32 -0500

    Removes script root subdirectory restriction (#3)

--- builds/build.sh
+++ builds/build.sh
@@ -1,5 +1,4 @@
-# docker build . -t build-s3fs
-
+docker build . -t build-s3fs
 s3fs_path=$(docker run --rm build-s3fs sh -c "which s3fs")
 id=$(docker create build-s3fs)
 docker cp $id:$s3fs_path ./builds/s3fs-$(date '+%Y-%m-%d-%H:%M:%S')
